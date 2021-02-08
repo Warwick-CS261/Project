e987ba13efcc78b98e286d0cfeb80b01d37a98d0
Eryl Joy <eryljoy29@googlemail.com>
2021-02-08 15:09:33 +0000

    added all enpoints mentioned in diagaram, basic output only

---
+++ app/src/main/java/cs261/Controllers/SessionController.java
@@ -0,0 +1,17 @@
+package cs261.Controllers;
+
+import java.util.*;
+import java.net.*;
+
+import spark.*;
+
+import java.io.File;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import cs261.*;;
+
+public class SessionController{
+
+
+
+}
---
+++ app/src/main/java/cs261/Session.java
@@ -0,0 +1,11 @@
+package cs261;
+
+public class Session {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
