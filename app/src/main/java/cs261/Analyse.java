5d8168bf640f6a68b9eb57d44f850d70837d0067
Jack Smith <mailjcsmith@gmail.com>
2021-02-12 18:45:40 +0000

    Analyse Class + Wordnet Dictionary
    Started to create the functions needed for the analyse class and added the wordnet dictionary

--- app/src/main/java/cs261/Analyse.java
+++ app/src/main/java/cs261/Analyse.java
@@ -7,6 +7,16 @@ public class Analyse {
 
     public static void main(String[] args) {
         
-        
     }
+
+    public float parseText(String data) {
+        String dataloc = "../dict/";
+
+        return 0.5f;
+    }
+
+    public float newMoodCoefficient(float oldMean, float newValue, int numValues) {
+        return ((oldMean * numValues) + newValue) / (numValues + 1);
+    }
+
 }
