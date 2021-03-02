8dc3b66e9df18bd58608c6758ffcf9f0a6cc0244
Jack Smith <mailjcsmith@gmail.com>
2021-03-02 11:38:08 +0000

    Initial setup

--- app/src/main/java/cs261/Analyse.java
+++ app/src/main/java/cs261/Analyse.java
@@ -4,6 +4,7 @@ import java.io.*;
 import javax.script.*;
 
 public class Analyse {
+<<<<<<< Updated upstream
 
     public static void main(String[] args) {
         // For Testing
@@ -11,6 +12,43 @@ public class Analyse {
 
     public float parseText(String data) {
         // Call Python Script
+=======
+
+    public static Invocable inv;
+
+    public static void main(String[] args) {
+        try {
+            init();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void init() throws Exception {
+        // All outputs from the python file are written to this StringWriter
+        StringWriter writer = new StringWriter();
+        ScriptContext context = new SimpleScriptContext();
+        context.setWriter(writer);
+    
+        // The engine to interpret the python scripts
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("python");
+        engine.setContext(context);
+    
+        // An object to interact with python functions
+        inv = (Invocable) engine;
+    
+        // Execute the python script
+        engine.eval(new FileReader("main.py"));
+    
+        //System.out.println(writer.toString().trim());
+    }
+
+    public static String callScript(String str) throws Exception {
+        return inv.invokeFunction("func1", str).toString();
+    }
+
+    public float parseText(String data) {
+>>>>>>> Stashed changes
 
         return 0.5f;
     }
