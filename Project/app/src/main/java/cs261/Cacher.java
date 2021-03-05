4a14c3f5246d1ea6d9366056f19ff69499517529
Eryl Joy <eryljoy29@googlemail.com>
2021-03-05 16:43:34 +0000

    Some more work on cacher

--- Project/app/src/main/java/cs261/Cacher.java
+++ Project/app/src/main/java/cs261/Cacher.java
@@ -1,19 +1,32 @@
 package cs261;
 
 import java.util.Queue;
+import java.util.Objects;
 import org.apache.commons.collections4.queue.CircularFifoQueue;
 
 public class Cacher {
-    DBConnection dbconn;
+    DBConnection dbConn;
     Queue<HostSesh> recentSessions;
 
 
     public Cacher(DBConnection dbConn){
-        this.dbconn = dbConn;
+        this.dbConn = dbConn;
         recentSessions = new CircularFifoQueue<HostSesh>(100);
     }
 
-    private HostSesh getHostSeshByID(String sessionID){
+
+    public HostSesh getHostSeshByID(String sessionID) throws Exception{
+        HostSesh hs;
+        if (Objects.isNull(hs = searchCache(sessionID))){
+            return hs;
+        }else{
+            return dbConn.getHostSessionByID(sessionID);
+        }
+    }
+
+    
+
+    private HostSesh searchCache (String sessionID){
         for(HostSesh hs : recentSessions){
             if (hs.getId().equals(sessionID)){
                 return hs;
@@ -21,9 +34,6 @@ public class Cacher {
         }
         return null;
     }
-
-    
-
 
 
 
