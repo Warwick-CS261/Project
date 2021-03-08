ebe7e6132f28e6ca948af7c3bfe5b4594841c0ce
Eryl Joy <eryljoy29@googlemail.com>
2021-03-08 21:59:38 +0000

    observable and watcher can differentiate

--- Project/app/src/main/java/cs261/Watcher.java
+++ Project/app/src/main/java/cs261/Watcher.java
@@ -4,19 +4,43 @@ public class Watcher {
 
     String json = "";
     int type = 0;
-    Boolean host = false;
+    Boolean requiresMod = false;
+    Boolean requiresAttendee = false;
+    Boolean both = false;
+    Boolean isMod = false;
 
-    public String watch(String sessionID, Boolean host) throws Exception{
-        this.host = host;
+    public String watch(String sessionID, Boolean isMod) throws Exception{
+        this.isMod = isMod;
         Object o = App.getApp().getObservable().addToList(sessionID, this);
         synchronized(o){
-            o.wait();
+            while(!(requiresMod && isMod)||!(requiresAttendee && !isMod)||!both){
+                o.wait();
+            }
         }
+        App.getApp().getObservable().removeFromList(sessionID, this);
         return json;
     }
 
-    public Boolean isHost(){
-        return host;
+    public void both(){
+        this.requiresAttendee = false;
+        this.requiresMod = false;
+        this.both = true;
+    }
+
+    public void requiresAttendee() {
+        this.requiresMod = false;
+        this.both = false;
+        this.requiresAttendee = true;
+    }
+
+    public void requiresMod() {
+        this.both = false;
+        this.requiresAttendee = false;
+        this.requiresMod = true;
+    }
+
+    public Boolean isMod(){
+        return isMod;
     }
 
     public int getType(){
