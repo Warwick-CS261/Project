5f17dc6b41f26c6e8cf104c5ca916e28f43ecb46
Eryl Joy <eryljoy29@googlemail.com>
2021-03-09 01:04:51 +0000

    de-insaneified Obseravle and Watcher

--- Project/app/src/main/java/cs261/Watcher.java
+++ Project/app/src/main/java/cs261/Watcher.java
@@ -4,39 +4,16 @@ public class Watcher {
 
     String json = "";
     int type = 0;
-    Boolean requiresMod = false;
-    Boolean requiresAttendee = false;
-    Boolean both = false;
     Boolean isMod = false;
 
     public String watch(String sessionID, Boolean isMod) throws Exception{
         this.isMod = isMod;
-        Object o = App.getApp().getObservable().addToList(sessionID, this);
+        Object o = App.getApp().getObservable().addToList(sessionID, this, isMod);
         synchronized(o){
-            while(!(requiresMod && isMod)&&!(requiresAttendee && !isMod)&&!both){
-                o.wait();
-            }
+            o.wait();
         }
-        App.getApp().getObservable().removeFromList(sessionID, this);
+        App.getApp().getObservable().removeFromList(sessionID, this, isMod);
         return json;
-    }
-
-    public void both(){
-        this.requiresAttendee = false;
-        this.requiresMod = false;
-        this.both = true;
-    }
-
-    public void requiresAttendee() {
-        this.requiresMod = false;
-        this.both = false;
-        this.requiresAttendee = true;
-    }
-
-    public void requiresMod() {
-        this.both = false;
-        this.requiresAttendee = false;
-        this.requiresMod = true;
     }
 
     public Boolean isMod(){
