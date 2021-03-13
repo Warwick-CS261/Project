96746f6f821bd03e6cab5ba4716d880e93786411
janoshr <janos.hajdurafis@gmail.com>
2021-03-13 18:55:12 +0000

    create question hidden under modal

--- Project/app/src/main/resources/static/js/components/question/CreateQuestion.js
+++ Project/app/src/main/resources/static/js/components/question/CreateQuestion.js
@@ -11,6 +11,7 @@ export default class CreateQuestion extends React.Component {
       pushed: false,
       error: false,
       general: false,
+      success: false,
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -64,6 +65,7 @@ export default class CreateQuestion extends React.Component {
         // TODO update session component with the question
         this.setState({
           question: '',
+          success: true,
         });
       },
       statusCode: {
@@ -90,53 +92,68 @@ export default class CreateQuestion extends React.Component {
   render() {
     return(
       <>
-        <h2>Create Question</h2>
-        {this.state.error !== false && 
-          <div className="alert alert-danger" role="alert">
-            {this.state.error}
+      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createQuestion">
+          Create Question
+        </button>
+        <div className="modal fade" id="createQuestion" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <form onSubmit={this.handleSubmit}>
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLabel">Create Question</h5>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                  {this.state.error !== false && 
+                    <div className="alert alert-danger" role="alert">
+                      {this.state.error}
+                    </div>
+                  }
+                  <div className="mb-3">
+                    <input 
+                      type="text"
+                      name="question"
+                      className="form-control"
+                      value={this.state.question}
+                      onChange={this.handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="mb-3">
+                    Push question immediately.
+                    <input
+                      type="checkbox"
+                      name="pushed"
+                      className="form-check-input"
+                      value={this.state.pushed}
+                      onChange={this.handleCheck}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    Include it in the general feedback
+                    <input
+                      type="checkbox"
+                      name="general"
+                      className="form-check-input"
+                      value={this.state.general}
+                      onChange={this.handleCheck}
+                    />
+                  </div>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button 
+                    type="submit"
+                    className="btn btn-primary"
+                    disabled={!this.state.question}
+                  >
+                    Create Question
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
-        }
-        <form onSubmit={this.handleSubmit}>
-          <div className="mb-3">
-            <input 
-              type="text"
-              name="question"
-              className="form-control"
-              value={this.state.question}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="mb-3">
-            Push question immediately.
-            <input
-              type="checkbox"
-              name="pushed"
-              className="form-check-input"
-              value={this.state.pushed}
-              onChange={this.handleCheck}
-            />
-          </div>
-          <div className="mb-3">
-            Include it in the general feedback
-            <input
-              type="checkbox"
-              name="general"
-              className="form-check-input"
-              value={this.state.general}
-              onChange={this.handleCheck}
-            />
-          </div>
-          <div className="mb-3">
-            <button 
-              type="submit"
-              className="btn btn-primary"
-              disabled={!this.state.question}
-            >
-              Create Question
-            </button>
-          </div>
-        </form>
+        </div>
       </>
     )
   }
